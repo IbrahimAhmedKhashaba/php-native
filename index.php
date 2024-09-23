063134754e20d702b2b540f895e7be4610eec202
Ibrahim Ahmed Khashaba <127418628+IbrahimAhmedKhashaba@users.noreply.github.com>
2024-09-23 03:17:10 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require_once __DIR__."/includes/app.php";
 ob_start();
+require_once __DIR__."/includes/app.php";
+session_save_path(config('session.session_save_path'));
+ini_set('session.gc_probability' , 1);
 session_start([
     'cookie_lifetime'=>config('session.expiration_timeout')
 ]);
